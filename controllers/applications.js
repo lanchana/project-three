24e62d2f1b95f0c41cc19273f267700ff2189427
lanchanashivananda <lanchanagupta@gmail.com>
2017-04-07 18:57:27 -0400

    set up our routes

---
+++ controllers/applications.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var applications = require('../models/application.js');
+var company = require('../models/company.js');
+// var bodyParser = require('body-parser');
+// var methodOverride = require('method-override');
+module.exports = router;

381d4e20db2387611495f0b19781fba5266b1d67
lanchanashivananda <lanchanagupta@gmail.com>
2017-04-08 10:58:18 -0400

    Add store get route

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var applications = require('../models/application.js');
-var company = require('../models/company.js');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
+var Company = require('../models/company.js');
+// Parses information from POST
+var bodyParser = require('body-parser');
+//  used to manipulate POST
+var methodOverride = require('method-override');
+
+router.get('/', (req, res) => {
+    Company.find({})
+            .exec((err, companies) => {
+                if(err) console.log(err);
+                res.json({companies: companies}).select('-__v');;
+            })
+});
+
 module.exports = router;

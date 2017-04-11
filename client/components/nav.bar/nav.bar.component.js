473dfe7dee859b024832c9b748f6a2bfaeb2b935
lanchanashivananda <lanchanagupta@gmail.com>
2017-04-10 23:13:39 -0400

    Add custom style

--- client/components/nav.bar/nav.bar.component.js
+++ client/components/nav.bar/nav.bar.component.js
@@ -1,1 +1,11 @@
-const controller = require('./login.controller.js');
+// const controller = require('./nav.bar.controller.js');
+// const template = require('./nav.bar.html');
+
+// const Component = {
+//     controller: controller,
+//     template: template
+// }
+
+// angular
+//     .module('projectThree')
+//     .component('navBar', Component);
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
     $(".button-collapse").sideNav();
     $('.tooltipped').tooltip({delay: 50});
+    $('.carousel').carousel();
+
 });
